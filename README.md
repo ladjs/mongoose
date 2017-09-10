679e2e0af4470d22bc99d4d6fdab5d79230d8337
Nick Baugh <niftylettuce@gmail.com>
2017-09-10 19:26:19 -0400

    Refactoring

--- README.md
+++ README.md
@@ -14,6 +14,7 @@
 
 * [Install](#install)
 * [Usage](#usage)
+* [How it works](#how-it-works)
 * [Contributors](#contributors)
 * [License](#license)
 
@@ -35,22 +36,120 @@ yarn add @ladjs/mongoose
 
 ## Usage
 
+> Basic usage:
+
 ```js
 const Mongoose = require('@ladjs/mongoose');
-const mongoose = new Mongoose(agenda, config, logger);
+const mongoose = new Mongoose().mongoose;
 ```
+
+> Advanced usage (showing default values):
+
+```js
+const Mongoose = require('@ladjs/mongoose');
+const Agenda = require('agenda');
+
+const mongoose = new Mongoose({
+
+  // `agenda` is not set by default but is accepted
+  // and by passing it here will connect agenda with
+  // existing mongoose connection db instance
+  agenda: new Agenda(),
+
+  // this field is optional, and only used when `agenda`
+  // option above is defined.  it is the collection name
+  // used by Agenda for storing jobs, and defaults to Agenda's default
+  agendaCollectionName: 'agendaJobs',
+
+  // this field is optional but should be used if `agenda` is
+  // it will basically call `agenda.every(interval, name)`
+  // upon database connection (and reconnection)
+  // (and of course when mongo disconnects it cancels these)
+  agendaRecurringJobs: [
+    // the definition below would be invoked as:
+    // `agenda.every('5 minutes', 'locales')`
+    [ '5 minutes', 'locales' ]
+  ],
+
+  // sets mongoose debug flag
+  debug: false,
+
+  // how often should we check to reconnect to mongo
+  reconnectMs: 3000,
+
+  // sets mongoose promise use
+  Promise: global.Promise,
+
+  // default logger, could be `new Logger()` from @ladjs/logger
+  logger: console,
+
+  // mongo connection options and database name
+  // note that you should replace "test" with your own
+  mongo: {
+    url: 'mongodb://localhost:27017/test',
+    options: {
+      // opt-in to the new connection logic for mongoose v4.11+
+      useMongoClient: true,
+      // the maximum number of tries to reconnect
+      reconnectTries: Number.MAX_VALUE
+    }
+  },
+
+  // these are the default options which get inherited from stop-agenda
+  // you do not have to pass these, but if you wish to customize them you can
+  // <https://github.com/ladjs/stop-agenda>
+  stopAgenda: {
+    logger: console,
+    cancelQuery: {
+      repeatInterval: {
+        $exists: true,
+        $ne: null
+      }
+    },
+    checkIntervalMs: 500
+  }
+}).mongoose;
+```
+
+
+## How it works
+
+When instantiated via `new Mongoose()`, this package will do the following:
+
+1. Create a listener for `mongoose.connection.on('connected')`
+2. Create a listener for `mongoose.connection.on('error')`
+3. Create a listener for `mongoose.connection.on('disconnected')`
+
+The `mongoose` global is exposed via `.mongoose` property of the instance `new Mongoose()`.
+
+Upon being instantiated, it will also do the following:
+
+1. Attempt to connect to MongoDB
+2. Re-use Mongoose's MongoDB connection to initialize Agenda with a collection of `config.agendaCollectionName`
+3. It will call `agenda.every(interval, name)` for any jobs defined in the associative array of `config.agendaRecurringJobs`
+4. Upon disconnect:
+
+* It will gracefully use [stop-agenda][] to stop agenda and cancel recurring jobs
+
+4. Upon reconnect
+
+* It will re-create via `agenda.every(interval, name)` all recurring jobs defined in `config.agendaRecurringJobs`
+* It will start agenda back up again with its initial max concurrency value
+
+Finally, throughout all of the above steps in this section it will be outputting info/errors using `config.logger`.
 
 
 ## Contributors
 
-| Name             | Website            |
-| ---------------- | ------------------ |
-| **Alexis Tyler** | <https://wvvw.me/> |
+| Name             | Website                   |
+| ---------------- | ------------------------- |
+| **Nick Baugh**   | <http://niftylettuce.com> |
+| **Alexis Tyler** | <https://wvvw.me/>        |
 
 
 ## License
 
-[MIT](LICENSE) © [Alexis Tyler](https://wvvw.me/)
+[MIT](LICENSE) © [Nick Baugh](http://niftylettuce.com)
 
 
 ## 
@@ -58,3 +157,5 @@ const mongoose = new Mongoose(agenda, config, logger);
 [npm]: https://www.npmjs.com/
 
 [yarn]: https://yarnpkg.com/
+
+[stop-agenda]: https://github.com/ladjs/stop-agenda
--- index.js
+++ index.js
@@ -1,135 +1,157 @@
-const async = require('async');
 const mongoose = require('mongoose');
 const delay = require('delay');
-
-// use native promises
-mongoose.Promise = global.Promise;
-
-const stopAgenda = (agenda, config, logger, fn) => {
-  // stop accepting new jobs
-  agenda.maxConcurrency(0);
-
-  async.parallel(
-    // TODO: do we need to handle results arg?
-    // <https://caolan.github.io/async/docs.html#reflectAll>
-    async.reflectAll([
-      fn => {
-        // cancel recurring jobs so they get redefined on next server start
-        // TODO: once PR is accepted we can take this out
-        // <https://github.com/agenda/agenda/pull/501>
-        if (!agenda._collection)
-          return fn(new Error('collection did not exist, see agenda#501'));
-
-        agenda.cancel(config.agendaCancelQuery, (err, numRemoved) => {
-          if (err) return fn(err);
-          logger.info(`cancelled ${numRemoved} jobs`);
-          fn();
-        });
-      },
-      fn => {
-        // check every 500ms for jobs still running
-        const jobInterval = setInterval(async () => {
-          if (agenda._runningJobs.length > 0) {
-            logger.info(`${agenda._runningJobs.length} jobs still running`);
-          } else {
-            clearInterval(jobInterval);
-            // cancel recurring jobs so they get redefined on next server start
-            // TODO: once PR is accepted we can take this out
-            // <https://github.com/agenda/agenda/pull/501>
-            if (!agenda._collection)
-              return fn(new Error('collection did not exist, see agenda#501'));
-            agenda.stop(fn);
-          }
-        }, 500);
-      }
-    ]),
-    fn
-  );
-};
-
-const reconnect = ({
-  mongodb,
-  mongodbOptions,
-  mongooseReconnectMs,
-  logger = console
-}) => {
-  return new Promise(async resolve => {
-    try {
-      await mongoose.connect(mongodb, mongodbOptions);
-      resolve();
-    } catch (err) {
-      logger.error(err);
-      logger.info(`attempting to reconnect in (${mongooseReconnectMs}) ms`);
-
-      await delay(mongooseReconnectMs);
-
-      resolve(reconnect());
-    }
-  });
-};
+const stopAgenda = require('stop-agenda');
 
 class Mongoose {
-  constructor(agenda, config = {}, logger = console) {
-    // create the database connection
-    mongoose.set('debug', config.mongooseDebug);
+  constructor(config) {
+    this.config = Object.assign(
+      {
+        agenda: false,
+        agendaCollectionName: 'agendaJobs',
+        agendaRecurringJobs: [],
+        debug: false,
+        reconnectMs: 3000,
+        Promise: global.Promise,
+        logger: console,
+        mongo: {},
+        stopAgenda: {}
+      },
+      config
+    );
+
+    this.logger = this.config.logger;
+    this.agenda = this.config.agenda;
+
+    this.config.mongo = Object.assign(
+      {
+        url: 'mongodb://localhost:27017/test',
+        options: {}
+      },
+      this.config.mongo
+    );
+
+    this.config.mongo.options = Object.assign(
+      {
+        useMongoClient: true,
+        reconnectTries: Number.MAX_VALUE
+      },
+      this.config.mongo.options
+    );
+
+    // set debug flag
+    mongoose.set('debug', this.config.debug);
+
+    // set promise type
+    mongoose.Promise = this.config.Promise;
 
     // when the connection is connected
-    mongoose.connection.on('connected', () => {
-      logger.info(`mongoose connection open to ${config.mongodb}`);
-
-      // When the connection is connected we need to override
-      // the default connection event, because agenda requires
-      // us to in order to connect with the same mongoose connection
-      if (agenda) {
-        // TODO: we need to define the recurring jobs here
-        // Re-use existing mongoose connection
-        agenda.mongo(
-          // <https://github.com/rschmukler/agenda/issues/156#issuecomment-163700272>
-          mongoose.connection.collection(config.agenda.collection).conn.db,
-          config.agenda.collection,
-          err => {
-            if (err) {
-              return logger.error(err);
-            }
-            // start accepting new jobs
-            agenda.maxConcurrency(config.agenda.maxConcurrency);
-            logger.info(
-              'agenda opened connection using existing mongoose connection'
-            );
-          }
-        );
-      }
-    });
+    mongoose.connection.on('connected', this.connected.bind(this));
 
     // if the connection throws an error
-    mongoose.connection.on('error', logger.error);
+    mongoose.connection.on('error', this.logger.error);
 
     // when the connection is disconnected
-    mongoose.connection.on('disconnected', () => {
-      logger.info('mongoose disconnected');
+    mongoose.connection.on('disconnected', this.disconnected.bind(this));
 
-      // Similarly when disconnected, we need to ensure that we stop agenda
-      if (agenda) {
-        stopAgenda(agenda, config, logger)
-          .then(() => {
-            return logger.info(
-              'gracefully stopped agenda due to mongoose disconnect'
-            );
-          })
-          .catch(err => {
-            return logger.error(err);
-          });
-      }
-    });
+    // if agenda was defined then ensure its a valid agenda instance
+    if (this.agenda) {
+      if (typeof this.config.agendaCollectionName !== 'string')
+        throw new Error('agneda collection name must be a String');
+
+      if (!Array.isArray(this.config.agendaRecurringJobs))
+        throw new Error('agenda recurring jobs must be an Array');
+
+      // if agenda was defined then store its max concurrency
+      // so that upon reconnection we can reset it back to normal
+      this.agendaMaxConcurrency = this.agenda._maxConcurrency;
+    }
 
     // connect to mongodb
-    // TODO: replace with top level await?
-    // TODO: async and await should not be used in a constructor
-    (async () => {
-      await reconnect();
-    })();
+    this.reconnect().then();
 
-    return mongoose;
+    // expose mongoose global
+    this.mongoose = mongoose;
+  }
+
+  reconnect() {
+    return new Promise(async resolve => {
+      try {
+        await mongoose.connect(
+          this.config.mongo.url,
+          this.config.mongo.options
+        );
+        resolve();
+      } catch (err) {
+        this.logger.error(err);
+        this.logger.info(
+          `attempting to reconnect in (${this.config.reconnectMs}) ms`
+        );
+        await delay(this.config.reconnectMs);
+        resolve(this.reconnect());
+      }
+    });
+  }
+
+  connected() {
+    this.logger.info(`mongoose connection open to ${this.config.mongo.url}`);
+
+    // When the connection is connected we need to override
+    // the default connection event, because agenda requires
+    // us to in order to connect with the same mongoose connection
+    if (!this.agenda) return;
+
+    // Re-use existing mongoose connection
+    this.agenda.mongo(
+      // <https://github.com/rschmukler/agenda/issues/156#issuecomment-163700272>
+      mongoose.connection.collection(this.config.agendaCollectionName).conn.db,
+      this.config.agendaCollectionName,
+      err => {
+        if (err) {
+          return this.logger.error(err);
+        }
+
+        // we need to redefine the recurring jobs here
+        // and it has to be inside `agenda.mongo` because
+        // otherwise `this._collection` is not defined yet
+        if (this.config.agendaRecurringJobs.length > 0) {
+          this.config.agendaRecurringJobs.forEach(def => {
+            if (!Array.isArray(def))
+              throw new Error('recurring job definition was not an array');
+            if (typeof def[0] !== 'string')
+              throw new Error(
+                'recurring job definition interval must be a String'
+              );
+            if (typeof def[1] !== 'string')
+              throw new Error('recurring job definition name must be a String');
+            this.logger.debug(
+              `scheduling every ${def[0]} the job named ${def[1]}`
+            );
+            this.agenda.every.apply(this.agenda, def);
+          });
+        }
+
+        // start accepting new jobs
+        this.agenda.maxConcurrency(this.agendaMaxConcurrency);
+        this.logger.info(
+          'agenda opened connection using existing mongoose connection'
+        );
+      }
+    );
+  }
+
+  disconnected() {
+    this.logger.info('mongoose disconnected');
+
+    if (!this.agenda) return;
+
+    // Similarly when disconnected, we need to ensure that we stop agenda
+    stopAgenda(this.config.stopAgenda)
+      .then(() => {
+        this.logger.info(
+          'gracefully stopped agenda due to mongoose disconnect'
+        );
+      })
+      .catch(this.logger.error);
   }
 }
 
