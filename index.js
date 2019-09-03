a77c7d243229d0f454fb59b2a5f3004d4dd22fdd
niftylettuce <niftylettuce@gmail.com>
2019-09-03 01:03:24 -0500

    fix: removed error message setting due to bug that still exists

--- index.js
+++ index.js
@@ -24,40 +24,8 @@ mongoose.configure = config => {
     },
     useCreateIndex: true,
     _connectionAttempts: 0,
-    errorMessages: {
-      general: {
-        default: '{PATH} was invalid; please contact us if necessary.',
-        required: '{PATH} is required.',
-        unique: '{PATH} of "{VALUE}" is not unique.'
-      },
-      Number: {
-        min:
-          '{PATH} of ({VALUE}) is less than minimum allowed value of ({MIN}).',
-        max: '{PATH} of ({VALUE}) is more than maximum allowed value ({MAX}).'
-      },
-      Date: {
-        min: '{PATH} of ({VALUE}) is before minimum allowed value ({MIN}).',
-        max: '{PATH} of ({VALUE}) is after maximum allowed value ({MAX}).'
-      },
-      String: {
-        enum: '{PATH} has an invalid selection of "{VALUE}".',
-        match: '{PATH} has an invalid value "{VALUE}".',
-        minlength:
-          '{PATH} of "{VALUE}" is shorter than the minimum allowed length ({MINLENGTH}).',
-        maxlength:
-          '{PATH} of "{VALUE}" is longer than the maximum allowed length ({MAXLENGTH}).'
-      }
-    },
     ...config
   };
-
-  // if there were error messages then set them
-  if (mongoose.config.errorMessages) {
-    mongoose.Error.messages = {
-      ...mongoose.Error.messages,
-      ...mongoose.config.errorMessages
-    };
-  }
 
   // if `useCreateIndex` was set as an option then use it
   mongoose.set('useCreateIndex', boolean(mongoose.config.useCreateIndex));
