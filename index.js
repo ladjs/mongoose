ecdef84712d227963e8e47fac3fe073153df2c41
niftylettuce <niftylettuce@gmail.com>
2019-09-08 23:00:05 -0500

    feat: refactored to be a class, added better tests and setup

--- index.js
+++ index.js
@@ -1,132 +1,125 @@
-const mongoose = require('mongoose');
-const delay = require('delay');
+const _ = require('lodash');
+const autoBind = require('auto-bind');
 const boolean = require('boolean');
+const delay = require('delay');
+const mongoose = require('mongoose');
 
-mongoose.configure = config => {
-  mongoose.config = {
-    agenda: false,
-    agendaCollectionName: process.env.AGENDA_COLLECTION_NAME || 'jobs',
-    debug: process.env.MONGOOSE_DEBUG
-      ? boolean(process.env.MONGOOSE_DEBUG)
-      : false,
-    logger: console,
-    mongo: {
-      uri: process.env.MONGO_URI || 'mongodb://localhost:27017/test',
-      options: {
-        reconnectTries: process.env.MONGO_RECONNECT_TRIES
-          ? parseInt(process.env.MONGO_RECONNECT_TRIES, 10)
-          : Number.MAX_VALUE,
-        reconnectInterval: process.env.MONGO_RECONNECT_INTERVAL
-          ? parseInt(process.env.MONGO_RECONNECT_INTERVAL, 10)
-          : 1000,
-        useNewUrlParser: true
-      }
-    },
-    useCreateIndex: true,
-    _connectionAttempts: 0,
-    ...config
-  };
+class Mongoose {
+  constructor(config = {}) {
+    this.config = _.merge(
+      {
+        logger: console,
+        debug: false,
+        mongo: {
+          options: {
+            reconnectTries: Number.MAX_VALUE,
+            reconnectInterval: 1000,
+            useNewUrlParser: true
+          }
+        },
+        useCreateIndex: true
+      },
+      config
+    );
 
-  // if `useCreateIndex` was set as an option then use it
-  mongoose.set('useCreateIndex', boolean(mongoose.config.useCreateIndex));
+    if (!_.isString(this.config.mongo.uri))
+      throw new Error('Missing `mongo.uri` (String) connection URI');
 
-  // set debug flag
-  mongoose.set('debug', boolean(mongoose.config.debug));
+    // store how many times we have attempted to connect/reconnect
+    this._connectionAttempts = 0;
 
-  // when the connection is connected
-  mongoose.connection.on('connected', mongoose.connected);
+    // <https://github.com/Automattic/mongoose/issues/8144>
+    // this._mongoose = new mongoose.Mongoose();
 
-  // if the connection throws an error
-  mongoose.connection.on('error', mongoose.config.logger.error);
+    // if `useCreateIndex` was set as an option then use it
+    mongoose.set('useCreateIndex', boolean(this.config.useCreateIndex));
 
-  // if agenda was defined then ensure its a valid agenda instance
-  if (mongoose.config.agenda) {
-    if (typeof mongoose.config.agendaCollectionName !== 'string')
-      throw new Error('agenda collection name must be a String');
-    mongoose.config.agenda._originalMaxConcurrency =
-      mongoose.config.agenda._maxConcurrency;
+    // set debug flag
+    mongoose.set('debug', boolean(this.config.debug));
+
+    // automatically bind this on methods
+    autoBind(this);
+
+    //
+    // connection events
+    // <https://mongoosejs.com/docs/connections.html#connection-events>
+    //
+    mongoose.connection.on('connecting', () =>
+      this.config.logger.debug('mongoose connection connecting')
+    );
+    mongoose.connection.on('connected', () =>
+      this.config.logger.debug('mongoose connection connected')
+    );
+    mongoose.connection.on('disconnecting', () =>
+      this.config.logger.debug('mongoose connection disconnecting')
+    );
+    mongoose.connection.on('disconnected', () =>
+      this.config.logger.debug('mongoose connection disconnected')
+    );
+    mongoose.connection.on('close', () =>
+      this.config.logger.debug('mongoose connection closed')
+    );
+    mongoose.connection.on('reconnected', () =>
+      this.config.logger.debug('mongoose connection reconnected')
+    );
+    mongoose.connection.on('fullsetup', () =>
+      this.config.logger.debug(
+        'mongoose connection replica set connected to primary server and at least one secondary server'
+      )
+    );
+    mongoose.connection.on('all', () =>
+      this.config.logger.debug(
+        'mongoose connection replica set connected to all servers'
+      )
+    );
+    mongoose.connection.on('reconnectFailed', () =>
+      this.config.logger.debug('mongoose connection reconnect failed')
+    );
+    mongoose.connection.on('reconnectTries', () =>
+      this.config.logger.debug('mongoose connection reconnect tries exceeded')
+    );
+    mongoose.connection.on('error', err => this.config.logger.error(err));
   }
-};
 
-mongoose._connect = mongoose.connect;
-mongoose.connect = async (
-  uri = mongoose.config.mongo.uri,
-  options = mongoose.config.mongo.options
-) => {
-  try {
-    await mongoose._connect(uri, options);
-    // output debug info
-    mongoose.config.logger.debug('mongo connected');
-    return;
-  } catch (err) {
-    mongoose.config._connectionAttempts++;
-    if (
-      mongoose.config._connectionAttempts >=
-      mongoose.config.mongo.options.reconnectTries
-    )
-      throw err;
-    mongoose.config.logger.warn(
-      `attempting to reconnect to mongo in ${
-        mongoose.config.mongo.options.reconnectInterval
-      } ms with ${mongoose.config.mongo.options.reconnectTries -
-        mongoose.config._connectionAttempts} retries remaining`
-    );
-    await delay(mongoose.config.mongo.options.reconnectInterval);
-    return mongoose.connect();
-  }
-};
-
-if (typeof mongoose.connected !== 'function')
-  mongoose.connected = () => {
-    // reset the times we've tried to reconnect
-    mongoose.config._connectionAttempts = 0;
-
-    // when the connection is disconnected
-    mongoose.connection.once('disconnected', mongoose.disconnected);
-
-    mongoose.config.logger.debug(
-      `mongoose connection open to ${mongoose.config.mongo.uri}`
-    );
-
-    // When the connection is connected we need to override
-    // the default connection event, because agenda requires
-    // us to in order to connect with the same mongoose connection
-    if (!mongoose.config.agenda) return;
-
-    // Re-use existing mongoose connection
-    mongoose.config.agenda.mongo(
-      // <https://github.com/rschmukler/agenda/issues/156#issuecomment-163700272>
-      mongoose.connection.collection(mongoose.config.agendaCollectionName).conn
-        .db,
-      mongoose.config.agendaCollectionName,
-      err => {
-        if (err) return mongoose.config.logger.error(err);
-
-        mongoose.config.agenda._isReady = true;
-
-        // start accepting new jobs
-        mongoose.config.agenda.maxConcurrency(
-          mongoose.config.agenda._originalMaxConcurrency
-        );
-        mongoose.config.logger.debug(
-          'agenda opened connection using existing mongoose connection'
-        );
-      }
-    );
-  };
-
-if (typeof mongoose.disconnected !== 'function')
-  mongoose.disconnected = async () => {
-    // stop accepting new jobs
-    if (mongoose.config.agenda) mongoose.config.agenda.maxConcurrency(0);
-    // output debug info
-    mongoose.config.logger.error('mongoose disconnected');
-    // attempt to reconnect
+  async connect(
+    uri = this.config.mongo.uri,
+    options = this.config.mongo.options
+  ) {
+    //
+    // NOTE: the reason why we have our own custom reconnect logic
+    // is that if the initial mongoose connection attempt fails
+    // then it doesn't attempt to connect again
+    // (e.g. connections are only reconnected after already established)
+    // and we want to continually retry even a connection from the start
+    //
     try {
-      await mongoose.connect();
+      await mongoose.connect(uri, options);
+      // reset the times we've tried to reconnect
+      this._connectionAttempts = 0;
     } catch (err) {
-      mongoose.config.logger.error(err);
+      this.config.logger.error(err);
+      this._connectionAttempts++;
+      if (this._connectionAttempts >= this.config.mongo.options.reconnectTries)
+        throw err;
+      this.config.logger.warn(
+        `attempting to reconnect to mongo in ${
+          this.config.mongo.options.reconnectInterval
+        } ms with ${this.config.mongo.options.reconnectTries -
+          this._connectionAttempts} retries remaining`
+      );
+      await delay(this.config.mongo.options.reconnectInterval);
+      return this.connect(uri, options);
     }
-  };
+  }
 
-module.exports = mongoose;
+  async disconnect() {
+    try {
+      // attempt to disconnect
+      await mongoose.disconnect();
+    } catch (err) {
+      this.config.logger.error(err);
+    }
+  }
+}
+
+module.exports = Mongoose;
--- test/test.js
+++ test/test.js
@@ -1,9 +1,25 @@
 const test = require('ava');
 
-const mongoose = require('..');
+const Mongoose = require('..');
 
-test('connects', async t => {
-  mongoose.configure();
-  await mongoose.connect();
+test('connects and disconnects', async t => {
+  const mongoose = new Mongoose({
+    mongo: { uri: 'mongodb://localhost:27017/test' }
+  });
+  await t.notThrowsAsync(mongoose.connect);
+  await t.notThrowsAsync(mongoose.disconnect);
   t.pass();
+});
+
+test('attempts to reconnect 3 times', async t => {
+  const mongoose = new Mongoose({
+    mongo: {
+      uri: 'mongodb://localhost:12345/test',
+      options: {
+        reconnectTries: 3,
+        reconnectInterval: 100
+      }
+    }
+  });
+  await t.throwsAsync(mongoose.connect);
 });
