49bd3e546376ca1365f163306d8906e0a8d61488
Spencer Snyder <sasnyde2@gmail.com>
2020-05-24 16:21:39 -0400

    fix: make mongoose 5.7+ compatible

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const delay = require('delay');
 const mongoose = require('mongoose');
+const { boolean } = require('boolean');
 
 class Mongoose {
   constructor(config = {}) {
@@ -19,6 +20,21 @@ class Mongoose {
       },
       config
     );
+
+    // detect mongoose version
+    this._mongooseVersion = mongoose.version;
+
+    if (this._mongooseVersion < '5')
+      throw new Error(
+        'mongoose peer dependency > 5 required.\nnpm install mongoose@5'
+      );
+
+    this.config.logger.debug(`using mongoose v${this._mongooseVersion}`);
+
+    // for mongoose v5.7 and higher
+    this._omitReconnectConnectionOptions =
+      this._mongooseVersion >= '5.7' &&
+      boolean(this.config.mongo.options.useUnifiedTopology);
 
     // store how many times we have attempted to connect/reconnect
     this._connectionAttempts = 0;
@@ -105,15 +121,24 @@ class Mongoose {
     // is that if the initial mongoose connection attempt fails
     // then it doesn't attempt to connect again
     // (e.g. connections are only reconnected after already established)
-    // and we want to continually retry even a connection from the start
-    //
+    // and we want to continually retry even a connection from the start.
+
+    // In v5.7, mongo driver is updated to 3.3 where some connection options were changed
+    // (https://github.com/Automattic/mongoose/blob/5.9.15/History.md#570--2019-09-09)
+    // (https://mongoosejs.com/docs/deprecations.html#useunifiedtopology)
+    // we now detect versions and omit options based on version
     try {
       if (!_.isString(uri))
         throw new Error(
           'Missing uri: pass a valid connection URI string as a parameter to `connect` or set the `mongo.uri` key in the mongoose constructor.'
         );
 
-      await mongoose.connect(uri, options);
+      let opts;
+      if (this._omitReconnectConnectionOptions)
+        opts = _.omit(options, ['reconnectInterval', 'reconnectTries']);
+      else opts = options;
+
+      await mongoose.connect(uri, opts);
       // reset the times we've tried to reconnect
       this._connectionAttempts = 0;
     } catch (err) {
